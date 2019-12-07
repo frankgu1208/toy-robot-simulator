b298a3a25cfb3a95f68e68a69c92cc23105592b2
Frank Gu <frank.gu@open.edu.au>
2019-12-08 10:36:12 +1100

    Implement the detail logic for tabletop

--- src/repository/tabletop.ts
+++ src/repository/tabletop.ts
@@ -1,3 +1,4 @@
+import * as config from '../constant/config';
 import { Position } from '../interface/position';
 
 export default class Tabletop {
@@ -8,12 +9,20 @@ export default class Tabletop {
         length: string | number = process.env.WIDTH,
         width: string | number = process.env.LENGTH,
     ) {
-        length = length;
-        width = width;
+        this.length = this.getTableSize(length);
+        this.width = this.getTableSize(width);
     }
 
     public isOnTable = (position: Position): boolean => {
-        position = position;
-        return undefined;
+        return Number.isInteger(position.x) && Number.isInteger(position.y)
+            && position.x >= 0 && position.x < this.length
+            && position.y >= 0 && position.y < this.width;
+    }
+
+    private getTableSize = (sizeString: number | string | undefined): number => {
+        const size = +sizeString;
+        return !!size && size > 0 && Number.isInteger(size)
+            ? size
+            : config.TABLETOP_DEAULT_SIZE;
     }
 }
