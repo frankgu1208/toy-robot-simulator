822ce87844bb2f71c24c8ecea645d4d4662c57c5
Frank Gu <frank.gu@open.edu.au>
2019-12-08 11:25:51 +1100

    Implement the detail for robot

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,4 +1,16 @@
+import { Direction } from './enums';
+
 /**
  * Tabletop default size: dimensions of 5 units * 5 units
  */
 export const TABLETOP_DEAULT_SIZE = 5;
+
+/**
+ * Direction order: for rotate the robot
+ */
+export const DIRECTION_ORDER: Direction[] = [
+    Direction.EAST,
+    Direction.SOUTH,
+    Direction.WEST,
+    Direction.NORTH,
+];
--- src/repository/robot.ts
+++ src/repository/robot.ts
@@ -1,36 +1,92 @@
+import { DIRECTION_ORDER } from '../constant/config';
+import { Command, Direction } from '../constant/enums';
 import { Position } from '../interface/position';
 import Action from './action';
 import Tabletop from './tabletop';
 
-export default class Robot {
+export default class RobotRepo {
     public position?: Position;
-    public tabletop: Tabletop;
 
-    public constructor(tabletop: Tabletop = new Tabletop()) {
-        tabletop = tabletop;
-    }
+    public constructor(public tabletop: Tabletop = new Tabletop()) { }
 
     public takeAction = (action: Action): void => {
-        action = action;
+        switch (action.type) {
+            case Command.PLACE:
+                return this.placeRobot(action);
+            case Command.MOVE:
+                return this.moveRobot(action);
+            case Command.RIGHT:
+                return this.turnRight(action);
+            case Command.LEFT:
+                return this.turnLeft(action);
+            case Command.REPORT:
+                return this.report(action);
+            default:
+                return undefined;
+        }
     }
 
     public placeRobot = (action: Action): void => {
-        action = action;
+        if (action.type === Command.PLACE && action.data
+            && this.tabletop.isOnTable(action.data)) {
+            this.position = action.data;
+        }
     }
 
     public moveRobot = (action: Action): void => {
-        action = action;
+        if (this.position && action.type === Command.MOVE) {
+            const {
+                x: roboX,
+                y: roboY,
+                direct,
+            } = this.position;
+            const nextPosition = {
+                ...this.position,
+                x: direct === Direction.EAST ? roboX + 1
+                    : (direct === Direction.WEST ? roboX - 1 : roboX),
+                y: direct === Direction.NORTH ? roboY + 1
+                    : (direct === Direction.SOUTH ? roboY - 1 : roboY),
+            } as Position;
+            if (this.tabletop.isOnTable(nextPosition)) {
+                this.position = nextPosition;
+            }
+        }
     }
 
     public turnLeft = (action: Action): void => {
-        action = action;
+        if (action.type === Command.LEFT) {
+            this.rotate(-1);
+        }
     }
 
     public turnRight = (action: Action): void => {
-        action = action;
+        if (action.type === Command.RIGHT) {
+            this.rotate(1);
+        }
     }
 
     public report = (action: Action): void => {
-        action = action;
+        if (action.type === Command.REPORT) {
+            if (this.position) {
+                const { x, y, direct } = this.position;
+                console.log(`${x},${y},${direct}`);
+            } else {
+                console.log('Robot is not on the table.');
+            }
+        }
+    }
+
+    private rotate = (times: number): void => {
+        if (this.position && DIRECTION_ORDER.indexOf(this.position.direct) >= 0) {
+            const nextIndex = ((DIRECTION_ORDER.indexOf(this.position.direct) + times)
+                % DIRECTION_ORDER.length + DIRECTION_ORDER.length) % DIRECTION_ORDER.length;
+            const nextDirect = DIRECTION_ORDER[nextIndex];
+            if (nextDirect) {
+                this.position = {
+                    ...this.position,
+                    direct: nextDirect,
+                };
+            }
+        }
     }
 }
